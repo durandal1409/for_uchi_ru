8f1438781f9eb4ad1f371166b498ee233d7a28a0
durandal1409 <mackladova@ya.ru>
2017-11-13 15:08:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,81 +1,78 @@
-        var canvas, ctx, w, h, aNum, sumNum, bNum, imageHeight, axisStartX, axisY, oneDivision, arrowOneEnd, arrowTwoStart;  
+                var canvas, ctx, w, h;
+        var aNum, sumNum, bNum; 
+        var imageHeight, axisStartX, axisY, oneDivision, arrowOneEnd, arrowTwoStart;  
 
 
-        // creating sprite image in canvas
+        // creating image element
         var img = document.createElement("img");
         img.src = "sprite.png";
 
         // after DOM loading calling init function
         window.onload = function init() {
-        canvas = document.getElementById("canv_1");
-  
-        w = canvas.width; 
-        h = canvas.height;  
-  
-        cx = canvas.getContext('2d');
-        // random numbers (a between [6,9], sum between [11,14])
-        aNum = Math.round((9 - 6) * Math.random() + 6);
-        sumNum = Math.round((14 - 11) * Math.random() + 11);
-        bNum = sumNum - aNum;
+          canvas = document.getElementById("canv_1");
+    
+          w = canvas.width; 
+          h = canvas.height;  
+    
+          cx = canvas.getContext('2d');
 
-        // adding numbers to expression
-        document.getElementById('aSpanID').innerHTML = aNum;
-        document.getElementById('bSpanID').innerHTML = bNum;
+          // random numbers (a between [6,9], sum between [11,14])
+          aNum = Math.round((9 - 6) * Math.random() + 6);
+          sumNum = Math.round((14 - 11) * Math.random() + 11);
+          bNum = sumNum - aNum;
 
-        
+          // putting numbers into expression
+          document.getElementById('aSpanID').innerHTML = aNum;
+          document.getElementById('bSpanID').innerHTML = bNum;        
 
-        // sprite width=w, height is proportional
-        imageHeight = w/875*83;
+          // img width=w, height is proportional
+          imageHeight = w/875*83;
 
-        // find axis start according to sprite width and height
-        axisStartX = w/24;
-        axisY = h - 0.75 * imageHeight;
-       
+          // find axis start according to img width and height
+          axisStartX = w/24;
+          axisY = h - 0.75 * imageHeight;
+         
 
-        // find one division of axis
-        oneDivision = w/22.5;
+          // find one division of axis
+          oneDivision = w/22.5;
 
-        // the end of first arrow and beginning of second arrow
-        arrowOneEnd = arrowTwoStart = axisStartX + oneDivision * aNum;
+          // the end of first arrow and beginning of second arrow
+          arrowOneEnd = arrowTwoStart = axisStartX + oneDivision * aNum;
           
           // drawing img
-          // x=0, y = (canvas height - sprite height), sprite width = canvas width, sprite height is proportional 
+          // x=0, y = (canvas height - img height), img width = canvas width, img height is proportional 
           cx.drawImage(img, 0, h-imageHeight, w, imageHeight);
           
+          // arrows style
           cx.strokeStyle = 'purple';
+          cx.lineWidth = 3;
           
-          // arrow1
+          // drawing arrow1
           drawArrow(axisStartX, arrowOneEnd, axisY);
 
-          // first input
+          // showing 'a' input
           numInput('aNumID', axisStartX, axisY, oneDivision, aNum);
           
         };    
 
-
-        function validityChecking(inputID, spanID, currentNum) {
-          
-          // add nextStep function as a parameter. For first validityChecking make func for arrow drawing and send it as nextStep parameter
-          // dont forget all slo's comments in skype
-          // dont forget to check in mozilla
+        // validity checking and following instructions for 'a' and 'b' inputs
+        function validityChecking(inputID, spanID, divID, currentNum, nextStep) {
 
           var currentElement = document.getElementById(inputID);
           var val = parseInt(currentElement.value);
           var currentSpan = document.getElementById(spanID);
+          var top = currentElement.style.top;
+          var left = currentElement.style.left;
 
           if (val === currentNum) {
-            console.log('correct!');
-            // showing first correct number
-            showingCorrectNumber('aNumDivID', axisStartX, axisY, oneDivision, currentNum);
+            // showing correct number
+            showingCorrectNumber(divID, currentNum, top, left);
             // changing span color
             currentSpan.style.backgroundColor = 'white';
+            // hiding input
             currentElement.style.display = 'none';
-
-            // arrow2
-            drawArrow(axisStartX + oneDivision * currentNum, axisStartX + oneDivision * currentNum + oneDivision * bNum, axisY);
-
-            // second input
-            numInput('bNumID', axisStartX + oneDivision*currentNum, axisY, oneDivision, bNum);
+            // next step (depending on input)
+            nextStep();
 
           }else {
             currentElement.style.color = 'red';
@@ -84,43 +81,31 @@
 
         };
 
-        function validityChecking2() {
-          
-          var currentElement = document.getElementById('bNumID');
-          var val = parseInt(currentElement.value);
-          var currentSpan = document.getElementById('bSpanID');
-
-          if (val === bNum) {
-            console.log('correct!');
-            // showing first correct number
-            showingCorrectNumber('bNumDivID', axisStartX + oneDivision*aNum, axisY, oneDivision, bNum);
-            // changing span color
-            currentSpan.style.backgroundColor = 'white';
-            currentElement.style.display = 'none';
-
-            // expression input
-            var inputElem = document.getElementById('sumNumID');
-            inputElem.style.display = 'inline-block';
-            inputElem.style.top = '20px';
-            inputElem.style.left = '540px';
-            inputElem.focus();
-
-          }else {
-            currentElement.style.color = 'red';
-            currentSpan.style.backgroundColor = 'orange';
-          };
-
+        function secondArrowAndInput() {
+          // arrow 'b'
+          drawArrow(arrowTwoStart, arrowTwoStart + oneDivision * bNum, axisY);
+          // input 'b'
+          numInput('bNumID', arrowTwoStart, axisY, oneDivision, bNum);
         };
 
-        function validityChecking3() {
+        function sumInput() {
+          // sum input
+          var inputElem = document.getElementById('sumNumID');
+          inputElem.style.display = 'inline-block';
+          inputElem.style.top = '20px';
+          inputElem.style.left = '540px';
+          inputElem.focus();
+        };
+
+        // validity checking and following instructions for 'sum' input
+        function validityCheckingSum() {
           
           var currentElement = document.getElementById('sumNumID');
           var val = parseInt(currentElement.value);
           var currentSpan = document.getElementById('sumSpanID');
 
           if (val === sumNum) {
-            console.log('correct!');
-            // showing first correct number
+            // showing correct number
             currentElement.style.display = 'none';
             currentSpan.innerHTML = sumNum;
             currentSpan.style.backgroundColor = 'white';
@@ -136,6 +121,7 @@
           cx.beginPath();
           cx.moveTo(fromX, Y);
           cx.quadraticCurveTo(fromX + (toX - fromX)/2, Y - (toX - fromX)/2, toX, Y);
+          console.log('quadraticCurveY', Y - (toX - fromX)/2);
           cx.stroke();
           // arrow for arc
           cx.beginPath();
@@ -148,17 +134,19 @@
         function numInput(inputID, axisX, axisY, oneDivision, currentNum) {
           var inputElem = document.getElementById(inputID);
           inputElem.style.display = 'inline-block';
-          inputElem.style.top = (axisY - oneDivision * currentNum/3)+'px';
+          // (axisY - oneDivision * currentNum/4) is the top of the curve, 25 - making input above the curve
+          inputElem.style.top = (axisY - oneDivision * currentNum/4 - 25)+'px';
+          console.log('numInputCoord', (axisY - oneDivision * currentNum/4 - 25));
           inputElem.style.left = (axisX + oneDivision * currentNum / 2 - 10)+'px';
           inputElem.focus();
         };
 
-        function showingCorrectNumber(correctNumberID, axisX, axisY, oneDivision, currentNum) {
+        function showingCorrectNumber(correctNumberID, currentNum, top, left) {
           var correctNumber = document.getElementById(correctNumberID);
           correctNumber.innerHTML = currentNum;
           correctNumber.style.visibility = 'visible';
-          correctNumber.style.top = (axisY - oneDivision * currentNum/3)+'px';
-          correctNumber.style.left = (axisX + oneDivision * currentNum / 2 - 10)+'px';
-        }
+          correctNumber.style.top = top;
+          correctNumber.style.left = left;
+        };
 
         
